5acad65f68e2345c38c6562ec5da4136e74fff3e
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-04-17 22:26:53 +0700

    feat : add asmaul husna

--- src/pages/AsmaulHusna.jsx
+++ src/pages/AsmaulHusna.jsx
@@ -1,5 +1,48 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import axios from "axios";
 
 export default function AsmaulHusna() {
-  return <div>AsmaulHusna</div>;
+  const [asmaulHusna, setAsmaulHusna] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get(`${import.meta.env.VITE_DZIKRAPI}/quran/asma`)
+      .then((res) => {
+        setAsmaulHusna(res.data.data);
+        console.log(res.data.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
+
+  return (
+    <div className="bg-gray-800 h-full p-4">
+      <div className="container mx-auto">
+        <h1 className="text-white text-4xl mb-4 text-center arab">
+          وَلِلَّهِ ٱلْأَسْمَآءُ ٱلْحُسْنَىٰ فَٱدْعُوهُ بِهَا ۖ وَذَرُوا۟
+          ٱلَّذِينَ يُلْحِدُونَ فِىٓ أَسْمَٰٓئِهِۦ ۚ سَيُجْزَوْنَ مَا كَانُوا۟
+          يَعْمَلُونَ
+        </h1>
+
+        <table className="w-full">
+          <tbody>
+            {asmaulHusna.map((item) => (
+              <tr key={item.id} className="text-center">
+                <td className="bg-white p-4 shadow text-gray-600 text-sm">
+                  {item.indo}
+                </td>
+                <td className="bg-white p-4 shadow text-gray-600 text-sm">
+                  {item.latin}
+                </td>
+                <td className="bg-white p-4 shadow text-lg font-semibold text-2xl">
+                  {item.arab}
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
+    </div>
+  );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { Link } from "react-router-dom";
 export default function Home() {
   return (
-    <div className="bg-gray-800">
+    <div className="bg-gray-800 h-screen">
       <div className="flex justify-center pt-8">
         <Link to={"/alquran"}>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded mx-2 text-center w-48 h-24">
@@ -11,7 +11,7 @@ export default function Home() {
         </Link>
         <Link to={"/doa"}>
           <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-4 px-8 rounded mx-2 w-48 h-24">
-            Doa Harian
+            Doa
           </button>
         </Link>
       </div>
