05255d279b79245b50725eadb1e14078e9c183b1
fahmi-azzuhri <fahmiazzuhriefki@gmail.com>
2024-04-25 00:00:59 +0700

    refactor : responsiv mobile

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,22 +9,22 @@ export default function Home() {
       <div className="absolute flex flex-col items-center mt-16">
         <div className="flex flex-row items-center flex-wrap justify-center mb-4">
           <Link to="/alquran">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24 ">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24 mt-1 md:mt-0">
               Alquran
             </button>
           </Link>
           <Link to="/doa">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24 mt-1 md:mt-0">
               Doa
             </button>
           </Link>
           <Link to="/asmaul-husna">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24 ">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24 mt-1 md:mt-0">
               Asmaul Husna
             </button>
           </Link>
           <Link to="/dzikir">
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold rounded mx-2 w-48 h-24 mt-1 md:mt-0">
               Dzikir
             </button>
           </Link>
